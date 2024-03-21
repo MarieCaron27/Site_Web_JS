b3b47d3ca5cf61c4f95e91fa781d931ce7e72619
MarieCaron27 <marie.caron@student.hepl.be>
2024-03-21 18:12:48 +0100

    Troisième partie serveur

--- app.js
+++ app.js
@@ -1,49 +1,36 @@
-const express = require("express"); //Gets the package express
-const app = express(); //Starts the server
+const express = require("express");
+const app = express();
 const mysql = require("mysql");
-const myPath = require("path"); //To import needed
+const path = require("path");
 
-console.log(myPath);
-
-const dataBase = mysql.createConnection
-({
+const dataBase = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'projet_web_jeux'
 });
 
-const publicDirectory = myPath.join(__dirname,'./public'); //Where our JS or CSS files are going to be
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'hbs');
 
-app.use(express.static(publicDirectory)); //Make sure that express uses the publicDirectory
-app.set('view engine','hbs');
-
-dataBase.connect((error) => 
-{
-    if(error)
-    {
+dataBase.connect((error) => {
+    if(error) {
         console.log(error);
-    }
-    else
-    {
+    } else {
         console.log("You are connected to the database !");
     }
 });
 
-app.get("/", (req,res) =>
-{
-    //res.send("<h1>Home Page</h1>"); //Send a responde to the requeste received (req)
-    res.render("index"); //Used to render a view and sends the rendered HTML string to the client
+app.get("/", (req, res) => {
+    res.render("index"); 
 });
 
-app.get("/", (req,res) =>
-{
-    //res.send("<h1>Home Page</h1>"); //Send a responde to the requeste received (req)
-    res.render("views/login_page"); //Used to render a view and sends the rendered HTML string to the client
+app.get("/login", (req, res) => {
+    res.render("login_page"); 
 });
 
-app.listen(8000, () =>
-{
+app.listen(8000, () => {
     console.log("Server started on port 8000");
 });
-
