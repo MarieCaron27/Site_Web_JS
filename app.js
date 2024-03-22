e9834d65ae226763423efe9b7629c9ad34f5d9b5
MarieCaron27 <marie.caron@student.hepl.be>
2024-03-22 12:18:09 +0100

    Réorganisation du site

--- app.js
+++ app.js
@@ -15,22 +15,34 @@ app.use(express.static(publicDirectory));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'hbs');
 
-dataBase.connect((error) => {
-    if(error) {
+dataBase.connect((error) => 
+{
+    if(error) 
+    {
         console.log(error);
-    } else {
+    } 
+    else 
+    {
         console.log("You are connected to the database !");
     }
 });
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => 
+{
     res.render("index"); 
 });
 
-app.get("/login", (req, res) => {
+app.get("/signUp", (req, res) => 
+{
+    res.render("signUp_page"); 
+});
+
+app.get("/login", (req, res) => 
+{
     res.render("login_page"); 
 });
 
-app.listen(8000, () => {
+app.listen(8000, () => 
+{
     console.log("Server started on port 8000");
 });
